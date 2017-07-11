8b735192b0e58a9224b3b915cfea27b7882675d8
xuzheng <xuzheng@benmu-health.com>
2017-07-11 10:25:11 +0800

    update Readme.md

--- src/main/java/com/xuen/lettuceredis/support/LettuceSupport.java
+++ src/main/java/com/xuen/lettuceredis/support/LettuceSupport.java
@@ -93,8 +93,7 @@ public class LettuceSupport {
         CompletionStage<Integer> future1 = CompletableFuture.supplyAsync(() -> 1);
         CompletionStage<Integer> future2 = CompletableFuture.supplyAsync(() -> 2);
         CompletionStage<Integer> future3 = CompletableFuture.supplyAsync(() -> 3);
-        ArrayList<CompletionStage<Integer>> inputs = Lists
-                .newArrayList(future1, future2, future3);
+        ArrayList<CompletionStage<Integer>> inputs = Lists.newArrayList(future1, future2, future3);
         CompletionStage<String> combine = LettuceSupport.combine(inputs, chain -> chain.get(0) + chain.get(1) + chain.get(2) + "");
         try {
             String s = LettuceSupport.get(combine);
