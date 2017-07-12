243928aa61d8111bac534206424f4e2df1b39e8d
xuzheng <xuzheng@benmu-health.com>
2017-07-12 18:01:16 +0800

    test metric

--- src/main/java/com/xuen/lettuceredis/bean/Server.java
+++ src/main/java/com/xuen/lettuceredis/bean/Server.java
@@ -1,5 +1,14 @@
 package com.xuen.lettuceredis.bean;
 
+import com.codahale.metrics.Metric;
+import com.xuen.metrics.MetricKey;
+import com.xuen.metrics.Metrics;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import metrics_influxdb.InfluxdbHttp;
+import metrics_influxdb.InfluxdbReporter;
+
+
 /**
  * @author zheng.xu
  * @since 2017-05-11
@@ -34,4 +43,17 @@ public class Server {
         this.port = port;
         return this;
     }
+
+    public static void main(String [] args){
+        Metrics.timer("sada").tag("a","a").get().update(200, TimeUnit.MILLISECONDS);
+        Metrics.timer("sada").tag("a","b").get().update(200, TimeUnit.MILLISECONDS);
+        Metrics.timer("sada").tag("a","c").get().update(200, TimeUnit.MILLISECONDS);
+        Map<MetricKey, Metric> metrics = Metrics.INSTANCE.getHolder().getMetrics();
+        for (Map.Entry<MetricKey, Metric> metric: metrics.entrySet()){
+            Metric value = metric.getValue();
+        }
+        System.out.println(metrics);
+
+    }
+
 }
